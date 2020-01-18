f4b3bbd883b545932d171ea5bdde917148f0527f
Alex Brachet <alexbrachetmialot@gmail.com>
2020-01-18 17:42:00 -0500

    Test opening libjava.so

--- test/unittests/VM/JNITest.cpp
+++ test/unittests/VM/JNITest.cpp
@@ -6,6 +6,7 @@
 #include "gtest/gtest.h"
 #include <cstdlib>
 #include <dlfcn.h>
+#include <functional>
 #include <unistd.h>
 
 TEST(JNI, LoadLibrary) {
@@ -19,4 +20,27 @@ TEST(JNI, CallFunction) {
   void *method = getMethodHandle("return5");
   ASSERT_TRUE(method);
   ASSERT_EQ(((int (*)())method)(), 5);
+}
+
+TEST(JNI, LoadLibjava) {
+  // TODO: libjvm is manually loaded here because loading libjava is dependent
+  // on libjvm. Add libjvm's path to the rpath of this executable.
+  void *handle = loadLibrary(LIBJVM_PATH);
+  ASSERT_TRUE(handle);
+  handle = loadLibrary(LIBJAVA_PATH);
+  ASSERT_TRUE(handle);
+  // Test istty because it doesn't need anything from the host jvm.
+  void *method = getMethodHandle("Java_java_io_Console_istty");
+  ASSERT_TRUE(method);
+  using IsttyT = bool(void);
+  std::function<IsttyT> istty = reinterpret_cast<IsttyT *>(method);
+  bool current = !!::isatty(1);
+  EXPECT_EQ(current, istty());
+  int newOut = ::dup(1);
+  (void)::close(1);
+  // Don't test these inline because gtest streams to stdout, not stderr.
+  current = !!::isatty(1);
+  bool jtty = istty();
+  (void)::dup(newOut);
+  EXPECT_EQ(current, jtty);
 }
