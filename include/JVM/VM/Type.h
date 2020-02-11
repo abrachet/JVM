6e3584d457ffd3ba7b174aaa6783c57fea05a6a3
Alex Brachet <alexbrachetmialot@gmail.com>
2020-02-11 00:31:44 -0500

    Remove #if 0'd block

--- include/JVM/VM/Type.h
+++ include/JVM/VM/Type.h
@@ -80,31 +80,4 @@ constexpr char Float('F');
 constexpr char Double('D');
 constexpr char Object('L');
 
-#if 0
-struct Type {
-  char c;
-
-  constexpr Type() : c(0) {}
-  constexpr Type(char c) : c(c) {}
-  size_t getStackEntryCount() const;
-  constexpr operator char() const { return c; }
-};
-
-constexpr Type Function('(');
-constexpr Type Void('V');
-constexpr Type Byte('B');
-constexpr Type Short('S');
-constexpr Type Int('I');
-constexpr Type Long('J');
-constexpr Type Char('C');
-constexpr Type Float('F');
-constexpr Type Double('D');
-constexpr Type Object('L');
-
-using TypeOrObject = std::pair<Type, std::string>;
-using FuncOrSingleType = std::pair<TypeOrObject, std::vector<TypeOrObject>>;
-
-ErrorOr<FuncOrSingleType> parseType(std::string_view);
-#endif
-
 #endif // JVM_VM_TYPEREADER_H
